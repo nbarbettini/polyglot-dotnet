c5226372549fcd4c809ce707b5efebb00157413b
Nate Barbettini <nathanaelb@gmail.com>
2015-12-03 15:20:51 -0800

    Add basic tests

---
+++ libpolyglot.Tests/Assembly_tests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Shouldly;
+using Xunit;
+
+namespace libpolyglot.Tests
+{
+    public class Assembly_tests
+    {
+        public static IEnumerable<object[]> TestAssemblies()
+        {
+            yield return new object[] { "EmptyVB.dll", Language.Vb };
+            yield return new object[] { "EmptyCS.dll", Language.CSharp };
+            yield return new object[] { "AnonymousAsyncVB.dll", Language.Vb };
+            yield return new object[] { "AnonymousAsyncCS.dll", Language.CSharp };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestAssemblies))]
+        public void When_analyzing(string file, Language expected)
+        {
+            var assembly = Assembly.LoadFrom($"TestAssemblies\\{file}");
+            var analyzer = new AssemblyAnalyzer(assembly);
+
+            analyzer.DetectedLanguage.ShouldBe(expected);
+        }
+    }
+}
