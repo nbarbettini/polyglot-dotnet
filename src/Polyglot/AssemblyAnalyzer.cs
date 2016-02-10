a2c8060b45fa45a8fe0f05431c7a3b643bb8e9d3
Nate Barbettini <nathanaelb@gmail.com>
2016-02-10 15:41:50 -0800

    Split GetReferencedAssemblies logic for NET451 and DNX

--- src/Polyglot/AssemblyAnalyzer.cs
+++ src/Polyglot/AssemblyAnalyzer.cs
@@ -4,7 +4,6 @@
 
 namespace libpolyglot
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
@@ -13,14 +12,16 @@ namespace libpolyglot
     /// <inheritdoc/>
     public sealed class AssemblyAnalyzer : IAssemblyAnalyzer
     {
+        private readonly bool failSilently;
         private readonly List<AnalysisResult> results;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AssemblyAnalyzer"/> class based on the given assembly.
         /// </summary>
         /// <param name="assemblyToAnalyze">The assembly to analyze.</param>
-        public AssemblyAnalyzer(Assembly assemblyToAnalyze)
+        public AssemblyAnalyzer(Assembly assemblyToAnalyze, bool failSilently = true)
         {
+            this.failSilently = failSilently;
             this.results = Analyze(assemblyToAnalyze).ToList();
         }
 
@@ -40,22 +41,31 @@ namespace libpolyglot
             return source?.Aggregate((l, r) => l.Score > r.Score ? l : r).Language;
         }
 
-        private static IEnumerable<AnalysisResult> Analyze(Assembly assembly)
+        private IEnumerable<AnalysisResult> Analyze(Assembly assembly)
         {
             try
             {
-                var referencedAssemblyNames = Enumerable.Empty<string>(); //todo
                 var internalTypeNames = assembly.DefinedTypes.Select(t => t.FullName);
+
+#if NET451
+                var referencedAssemblyNames = assembly.GetReferencedAssemblies().Select(x => x.Name);
+#else
+                var referencedAssemblyNames = Enumerable.Empty<string>(); // not currently supported :(
+#endif
 
                 var analysisData = new AnalysisData(referencedAssemblyNames, internalTypeNames);
 
                 var runner = new HeuristicRunner(HeuristicProvider.GetAll());
                 return runner.GetResults(analysisData);
             }
-            catch (Exception e)
+            catch
             {
-                // log?
-                return null;
+                if (this.failSilently)
+                {
+                    return null;
+                }
+
+                throw;
             }
         }
     }
